ce326a42348916d19127a212da1757d4cfdf766e
Matt DeBoard <matt.deboard@gmail.com>
2014-01-26 20:29:39 -0500

    Add short_name field to category.

--- fuselage/models.py
+++ fuselage/models.py
@@ -27,6 +27,7 @@ class Article(models.Model):
 class Category(models.Model):
     seats = models.IntegerField()
     title = models.CharField(max_length=20, db_index=True, unique=True)
+    short_name = models.CharField(max_length=20, db_index=True, unique=True)
     priority = models.IntegerField()
 
     class Meta:
