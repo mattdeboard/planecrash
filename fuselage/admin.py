97102f13f5b858d8a1f649db3ce5c7f37baa497e
Matt DeBoard <matt.deboard@gmail.com>
2014-01-27 19:55:53 -0500

    Add ordering for models in the admin panel.

--- fuselage/admin.py
+++ fuselage/admin.py
@@ -7,11 +7,13 @@ class ArticleAdmin(admin.ModelAdmin):
     list_display = ('category', 'priority', 'original_headline', 'created')
     search_fields = ('original_headline', 'url', 'category__title')
     list_filter = ('category__title',)
+    ordering = ('category__priority', 'priority')
 
 admin.site.register(Article, ArticleAdmin)
 
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('title', 'priority', 'short_name', 'seats')
+    ordering = ('priority',)
 
 admin.site.register(Category, CategoryAdmin)
